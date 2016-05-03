21af7526d3f985ab9660f5cea080d3dc2b17e1f2
“Krisgit config --global user.email “squeakyalien98@gmail.com <Krisbies@gmail.com>
2016-05-02 19:55:13 -0500

    trial update

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,18 @@
 class StudentsController < ApplicationController
-
   def index
     render 'index.html.erb'
+  end
+
+  def show
+    @users = User.all
+    render 'show.html.erb'
+  end
+
+  def edit
+    render 'edit.html.erb'
+  end
+
+  def update
+    rendder 'edit.html.erb'
   end
 end
