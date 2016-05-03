a64b44420cb662bf27caba305e3e92fd52d4ba3d
“Krisgit config --global user.email “squeakyalien98@gmail.com <Krisbies@gmail.com>
2016-05-02 21:24:40 -0500

    edit, update, create forms and routes

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,15 +4,45 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @users = User.all
+    @user = User.find_by(id: params[:id])
     render 'show.html.erb'
   end
 
   def edit
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
     render 'edit.html.erb'
   end
 
   def update
-    rendder 'edit.html.erb'
+    user_id = params[:id]
+    @user = User.find_by(id: user_id)
+    @user.update(
+      first_name: params[:first_name], 
+      last_name: params[:last_name], 
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:age],
+      blog: params[:blog],
+      resume_url: params[:resume_url],
+      github_url: params[:github_url],
+      photo: params[:photo],
+      experience: params[:experience],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      details: params[:details],
+      university_name: params[:university_name],
+      degree: params[:degree],
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      details: params[:details],
+      name: params[:name]
+    )
+    flash[:success] = "Resume Updated!"
+    redirect_to "/paws/#{@paw.id}"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/' => 'students#index'
   get '/students' => 'students#index'
   get '/students/:id' => 'students#show'
+  get '/students/:id/edit' => 'students#edit'
+  post '/students/' => 'students#update'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
