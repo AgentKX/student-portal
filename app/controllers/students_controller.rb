9278cba3566226f968e3362ea8972ef55113609e
Paul <paulyk1983@gmail.com>
2016-05-02 19:37:07 -0500

    added code to test Unirest api call

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
-
   def index
+    @crimes = Unirest.get("https://data.cityofchicago.org/resource/ijzp-q8t2.json").body
     render 'index.html.erb'
   end
 end
