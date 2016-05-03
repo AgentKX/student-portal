1e0d5da83ba2a2dccdc29b2048decda2111c04cd
Paul <paulyk1983@gmail.com>
2016-05-02 19:42:30 -0500

    deleted unirest test api call

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,5 @@
 class StudentsController < ApplicationController
   def index
-    @crimes = Unirest.get("https://data.cityofchicago.org/resource/ijzp-q8t2.json").body
     render 'index.html.erb'
   end
 end
