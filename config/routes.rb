fd740c45f9e44432de01a92f35930210d5b81015
Zeina <zeemakky@gmail.com>
2016-05-03 20:27:37 -0500

    Fixed the edit page. Started work on update action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get '/students' => 'students#index'
   get '/students/:id' => 'students#show'
   get '/students/:id/edit' => 'students#edit'
-  post '/students/' => 'students#update'
+  patch '/students/:id' => 'students#update'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
